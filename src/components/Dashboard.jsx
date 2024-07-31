d81fa5c212e7364eabe1e18c4cfc145b559e911e
Miriam <m4140531@gmail.com>
2024-07-31 13:44:06 +0300

    ffl;fl'

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -34,7 +34,7 @@ const Dashboard = () => {
     //צריך להוסיף לשליחה גם אפשרות של targets
     const data = await addLinkForUser(originalUrl,userId)
     console.log("dataaaaaaaakululululu",data);
-    setShortUrl(`https://tinyurlapp.onrender.com/${data.id}`)
+    setShortUrl(`tinyurl-n41k.onrender.com/${data.id}`)
   }
   return (
     <div>
